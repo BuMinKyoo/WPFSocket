470feaf4f039c08a40a58356d553b5be626e2d31
BuMinKyoo <qnalsrb1@naver.com>
2023-12-04 00:22:50 +0900

    GET,POST 분리 및 파싱

--- WPFSocketServer/MainWindow.xaml.cs
+++ WPFSocketServer/MainWindow.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading;
+using System.Text.Json.Nodes;
 using System.Windows;
 
 namespace WPFSocketServer
@@ -70,15 +72,36 @@ namespace WPFSocketServer
 
             string receivedData = Encoding.UTF8.GetString(buffer); // 바이트 배열을 문자열로 변환
 
-            SendData(obj.WorkingSocket);
+            if (receivedData.StartsWith("GET"))
+            {
+                // 경로 추출
+                string[] requestLines = receivedData.Split('\r', '\n');
+                string[] requestParts = requestLines[0].Split(' ');
+                if (requestParts.Length >= 2)
+                {
+                    string path = requestParts[1]; // "/path/to/resource"
+
+                    // get으로 왔을시 응답
+                    HandleGetRequestToRespons(obj.WorkingSocket, path);
+                }
+            }
+            else if (receivedData.StartsWith("POST"))
+            {
+                int bodyStartIndex = receivedData.IndexOf("\r\n\r\n") + 4;
+                string jsonBody = receivedData.Substring(bodyStartIndex);
+
+                // post로 왔을시 응답
+                HandlePostRequestToRespons(obj.WorkingSocket, jsonBody);
+            }
+            else
+            {
+                Debug.WriteLine("Unsupported request type");
+            }
         }
 
-        private void SendData(Socket workingSocket)
+        private void HandleGetRequestToRespons(Socket workingSocket, string requestPath)
         {
-            //string str = "Hello World";
-            //var data = Encoding.UTF8.GetBytes(str.ToString());
-
-            string strSendData = "요야유";
+            string strSendData = "";
             System.Globalization.CultureInfo culInfo = new System.Globalization.CultureInfo("en-US");
 
             // http프로토콜로 보내기
@@ -86,8 +109,22 @@ namespace WPFSocketServer
             sb.AppendLine("HTTP/1.1 200 ok");
             sb.AppendLine("date: " + DateTime.UtcNow.ToString("ddd, dd MMM yyy HH':'mm':'ss 'GMT'", culInfo));
             sb.AppendLine("server: test server");
-            sb.AppendLine("Content-Length: " + (Encoding.UTF8.GetBytes(strSendData)).Length);
             sb.AppendLine("content-type:text/html; charset=UTF-8");
+
+            if (requestPath == "/some/resource")
+            {
+                strSendData = "요야유";
+            }
+            else if (requestPath == "/another/resource")
+            {
+
+            }
+            else
+            {
+                strSendData = "요야유";
+            }
+
+            sb.AppendLine("Content-Length: " + (Encoding.UTF8.GetBytes(strSendData)).Length);
             sb.AppendLine();
             sb.AppendLine(strSendData);
             var bytes = Encoding.UTF8.GetBytes(sb.ToString());
@@ -96,7 +133,67 @@ namespace WPFSocketServer
             {
                 // 데이터를 클라이언트에게 보냅니다.
                 int bytesSent = workingSocket.Send(bytes);
-                Thread.Sleep(10);
+                //Thread.Sleep(10);
+
+                // 데이터 전송 성공 여부 확인
+                if (bytesSent == bytes.Length)
+                {
+                    Debug.WriteLine("데이터를 클라이언트에게 성공적으로 보냈습니다.");
+                }
+                else
+                {
+                    Debug.WriteLine("데이터 전송이 부분적으로 완료되었습니다.");
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine($"데이터 전송 중 오류 발생: {e.Message}");
+            }
+        }
+
+        private void HandlePostRequestToRespons(Socket workingSocket, string jsonBody)
+        {
+            string strSendData = "";
+            System.Globalization.CultureInfo culInfo = new System.Globalization.CultureInfo("en-US");
+
+            // http프로토콜로 보내기
+            StringBuilder sb = new StringBuilder(100);
+            sb.AppendLine("HTTP/1.1 200 ok");
+            sb.AppendLine("date: " + DateTime.UtcNow.ToString("ddd, dd MMM yyy HH':'mm':'ss 'GMT'", culInfo));
+            sb.AppendLine("server: test server");
+            sb.AppendLine("content-type:text/html; charset=UTF-8");
+
+            JArray jsonArray = null;
+
+            // JSON 파싱
+            try
+            {
+                jsonArray = JArray.Parse(jsonBody);
+                // JSON을 객체로 파싱하는 예시 (Newtonsoft.Json 라이브러리 사용)
+                //YourJsonObject jsonObject = JsonConvert.DeserializeObject<YourJsonObject>(jsonBody);
+            }
+            catch (JsonException ex)
+            {
+                Debug.WriteLine($"JSON 파싱 오류: {ex.Message}");
+                // JSON 파싱 오류 처리
+            }
+
+            JObject jsonObject1 = jsonArray[0].ToObject<JObject>();
+            int id1 = jsonObject1["id"].ToObject<int>();
+            string name1 = jsonObject1["name"].ToObject<string>();
+            string email1 = jsonObject1["email"].ToObject<string>();
+            strSendData = "POST통신";
+
+            sb.AppendLine("Content-Length: " + (Encoding.UTF8.GetBytes(strSendData)).Length);
+            sb.AppendLine();
+            sb.AppendLine(strSendData);
+            var bytes = Encoding.UTF8.GetBytes(sb.ToString());
+
+            try
+            {
+                // 데이터를 클라이언트에게 보냅니다.
+                int bytesSent = workingSocket.Send(bytes);
+                //Thread.Sleep(10);
 
                 // 데이터 전송 성공 여부 확인
                 if (bytesSent == bytes.Length)
